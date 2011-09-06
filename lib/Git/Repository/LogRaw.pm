d0e8bbde6cec009fe76fd943a1572bd4e81361c5
Michal Jurosz <mj@mj41.cz>
2011-09-06 20:42:31 +0200

    --numstat parsing

--- lib/Git/Repository/LogRaw.pm
+++ lib/Git/Repository/LogRaw.pm
@@ -17,20 +17,18 @@ sub new {
         # todo
         #require 'Data::Dumper';
     }
-    
+
     bless $self, $class;
     return $self;
 }
 
 
-sub parse_item_log_line {
-    my ( $self, $line ) = @_;
+sub parse_one_item_begin {
+    my ( $self, $line, $item_num ) = @_;
 
-    my $colons = undef;
-    unless ( ($colons) = $line =~ m/^(\:+)/g ) {
-        return "Colons not found on begin of line";
-    }
-    
+    print "parsing item num $item_num: '$line'\n" if $self->{ver} >= 6;
+    return "Colons not found on begin of item $item_num" unless $line =~ m/^(\:+)/gcx;
+    my $colons = $1;
     my $last_pnum = length( $colons ) - 1;
 
     my $item_info = {
@@ -48,49 +46,91 @@ sub parse_item_log_line {
         };
     }
 
-    my $val;
-
     # mode
     foreach my $pnum (0..$last_pnum) {
-        unless ( ($val) = $line =~ m/ ([0-7]{6})/g ) {
-            return "Mode for parent number ".($pnum+1)." not found";
-        }
-        $item_info->{parents}->[ $pnum ]->{mode} = $val;
+        # No space before the first parent mode.
+        return "Mode for parent number ".($pnum+1)." not found" unless $line =~ m/\G \s? ([0-7]{6}) /gcx;
+        $item_info->{parents}[ $pnum ]{mode} = $1;
     }
-    unless ( ($val) = $line =~ m/ ([0-7]{6})/g ) {
-        return "Mode for new item not found";
-    }
-    $item_info->{mode} = $val;
+    return "Mode for new item not found" unless $line =~ m/\G \s ([0-7]{6}) /gcx;
+    $item_info->{mode} = $1;
+
 
     # hash
     foreach my $pnum (0..$last_pnum) {
-        unless ( ($val) = $line =~ m/ ([0-9a-f]{40})/g ) {
-            return "Hash for parent number ".($pnum+1)." not found";
-        }
-        $item_info->{parents}->[$pnum]->{hash} = $val;
+        return "Hash for parent number ".($pnum+1)." not found" unless $line =~ m/\G \s ([0-9a-f]{40}) /gcx;
+        $item_info->{parents}[ $pnum ]{hash} = $1;
     }
-    unless ( ($val) = $line =~ m/ ([0-9a-f]{40})/g ) {
-        return "Hash for new item not found";
-    }
-    $item_info->{hash} = $val;
+    return "Hash for new item not found" unless $line =~ m/(\G \s ([0-9a-f]{40}) )/gcx;
+    $item_info->{hash} = $1;
+
 
     # status
+    return "Empty char before status (change char) not found" unless $line =~ m/\G \s /gcx;
     foreach my $pnum (0..$last_pnum) {
-        unless ( ($val) = $line =~ m/ ([MAD])/g ) {
-            return "Status (change char) parent number ".($pnum+1)." not found";
-        }
-        $item_info->{parents}->[$pnum]->{status} = $val;
+        return "Status (change char) parent number ".($pnum+1)." not found" unless $line =~ m/\G ([MAD]) /gcx;
+        $item_info->{parents}[ $pnum ]{status} = $1;
     }
 
     # name
-    unless ( ($val) = $line =~ m/\t(.+)$/g ) {
-        return "Name not found";
-    }
-    $item_info->{name} = $val;
-
+    return "Name not found" unless $line =~ m/\G \0 ([^\0]+) \0? (.*?) $/gcx;
+    $item_info->{name} = $1;
+    my $next_item_str = $2;
 
     #print Dumper( $item_info );
-    return $item_info; 
+    return $item_info, $next_item_str;
+}
+
+
+sub parse_one_item_stat_begin {
+    my ( $self, $line, $stat_num ) = @_;
+
+    print "parsing stat num $stat_num: '$line'\n" if $self->{ver} >= 6;
+
+    my $stat = {};
+    return "Added and removed lines numbers not found of stat item $stat_num" unless $line =~ m/\G (\d+|\-) \t (\d+|\-) \t /gcx;
+    my ( $added, $removed ) = ( $1, $2 );
+    $stat->{lines_added} = $added eq '-' ? undef : $added;
+    $stat->{lines_removed} = $removed eq '-' ? undef : $removed;
+
+    return "File name not found of stat item $stat_num" unless $line =~ /\G ([^\0]+) (?:\0(.+))? $/gcx;
+    my $fname = $1;
+    return ( $fname, $stat, $2 ) if $2;
+    return ( $fname, $stat );
+}
+
+
+sub parse_items_log_line {
+    my ( $self, $items_str ) = @_;
+
+    my @items_info;
+    my %items_stat;
+    my $item_num = 1;
+    my $stat_num = 1;
+    my $new_items_str;
+    while ( $items_str ) {
+
+        if ( $items_str =~ /^\:/ ) {
+            my $item;
+            ( $item, $new_items_str ) = $self->parse_one_item_begin( $items_str, $item_num );
+            return $item unless ref $item eq 'HASH';
+            push @items_info, $item;
+            last unless $items_str;
+            $item_num++;
+
+        } else {
+            my ( $fname, $item_stat );
+            ( $fname, $item_stat, $new_items_str ) = $self->parse_one_item_stat_begin( $items_str, $stat_num );
+            return $fname unless ref $item_stat eq 'HASH';
+            $items_stat{ $fname } = $item_stat;
+            last unless $items_str;
+            $stat_num++;
+        }
+
+        $items_str = $new_items_str;
+    }
+
+    return \@items_info, \%items_stat;
 }
 
 
@@ -107,11 +147,11 @@ sub parse_person_log_line_part {
 
 sub get_log {
     my ( $self, $ssh_skip_list ) = @_;
-    
-    my $cmd = $self->{repo}->command( 'log' => '--date-order', '--reverse', '--all', '--pretty=raw', '--raw', '-c', '-t', '--root', '--abbrev=40' );
+
+    my $cmd = $self->{repo}->command( 'log' => '--numstat', '--date-order', '--reverse', '--all', '--pretty=raw', '--raw', '-c', '-t', '--root', '--abbrev=40', '-z' );
     print "LogRaw cmdline: '" . join(' ', $cmd->cmdline() ) . "'\n" if $self->{ver} >= 4;
 
-    
+
     my $line_num = 0;
     my $log = [];
     my $ac_state = 'begin';
@@ -122,21 +162,31 @@ sub get_log {
     PARSE_LOG: while ( my $line = <$out_fh> ) {
         $line_num++;
         chomp $line;
-        printf( "%3d (prev %10s): '%s'\n", $line_num, $ac_state, $line ) if $self->{ver} >= 5;
+        printf( "%3d (prev %10s): '%s'\n", $line_num, $ac_state, $line ) if $self->{ver} >= 7;
 
         # commit 1x
-        if ( my ( $commit_hash ) = $line =~ /^commit ([0-9a-f]{40})$/ ) {
+        PARSE_COMMIT_LINE:
+        print "on commit: $line\n" if $self->{ver} >= 6;
+        if ( my ( $nulls, $commit_hash ) = $line =~ /^(\0{0,2})commit ([0-9a-f]{40})$/ ) {
             if ( defined $commit ) {
-                print Dumper( $commit ) if $self->{ver} >= 5;
+                print Dumper( $commit ) if $self->{ver} >= 8;
                 if ( (not defined $ssh_skip_list) || (not exists $ssh_skip_list->{$commit->{commit}}) ) {
                     push @$log, $commit;
                 }
             }
 
+            if ( $ac_state eq 'commit_after_items' ) {
+                if ( $nulls ne '' ) {
+                    $err_msg = "Found 'commit' begining with " . length($nulls) . " null(s) after '$ac_state'.";
+                    last PARSE_LOG;
+                }
 
-            if ( $ac_state ne 'begin' && $ac_state ne 'empty_ca' && $ac_state ne 'empty_af' ) {
+            } elsif ( $ac_state eq 'msg' && ($nulls eq "\0" || $nulls eq "\0\0") ) {
+                # todo - one or two nulls on merge commits are ok?
+
+            } elsif ( $ac_state ne 'begin' && $ac_state ne 'empty_ca' && $ac_state ne 'empty_af' ) {
                 $err_msg = "Found 'commit' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             $commit = {
                 commit => $commit_hash,
@@ -165,7 +215,7 @@ sub get_log {
         if ( my ( $tree ) = $line =~ /^tree ([0-9a-f]{40})$/ ) {
             if ( $ac_state ne 'commit' ) {
                 $err_msg = "Found 'tree' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             $commit->{tree} = $tree;
             $ac_state = 'tree';
@@ -176,7 +226,7 @@ sub get_log {
         if ( my ( $parent ) = $line =~ /^parent ([0-9a-f]{40})$/ ) {
             if ( $ac_state ne 'tree' && $ac_state ne 'parent' ) {
                 $err_msg = "Found 'parent' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             push @{$commit->{parents}}, $parent;
             $ac_state = 'parent';
@@ -187,7 +237,7 @@ sub get_log {
         if ( my ( $person_raw ) = $line =~ /^author (.*)$/ ) {
             if ( $ac_state ne 'parent' && $ac_state ne 'tree' ) {
                 $err_msg = "Found 'author' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             my ( $status, $name, $email, $timezone, $gmtime ) = $self->parse_person_log_line_part( $person_raw );
             if ( $status != 1 ) {
@@ -206,7 +256,7 @@ sub get_log {
         if ( my ( $person_raw ) = $line =~ /^committer (.*)$/ ) {
             if ( $ac_state ne 'author' ) {
                 $err_msg = "Found 'committer' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             my ( $status, $name, $email, $timezone, $gmtime ) = $self->parse_person_log_line_part( $person_raw );
             if ( $status != 1 ) {
@@ -241,31 +291,41 @@ sub get_log {
             $err_msg = "Found empty line after '$ac_state'";
             last PARSE_LOG;
         }
-        
+
         # msg/msg line - 1+x
         if ( my ( $msg_line ) = $line =~ /^ {4}(.*)$/ ) {
             if ( $ac_state ne 'empty_cb' && $ac_state ne 'msg' ) {
                 $err_msg = "Found 'msg' type line after '$ac_state'.";
-                last PARSE_LOG; 
+                last PARSE_LOG;
             }
             $commit->{msg} .= "\n" if $commit->{msg};
             $commit->{msg} .= $msg_line;
             $ac_state = 'msg';
             next;
         }
-        
+
         # empty_ca 1x - code is above
 
         # items 0+x
-        if ( $line =~ /^\:+/ ) {
-            my $item_info = $self->parse_item_log_line( $line );
-            if ( ref $item_info ne 'HASH' ) {
-                $err_msg = "Item line error. $item_info.";
+        if ( $line =~ /^\0?\:+/ || $line =~ /^\0(\d+|\-)\t(\d+|\-)\t/ ) {
+            my ( $items_part, $commit_part ) = split '\0\0', $line;
+            $items_part =~ s{^\0}{};
+            $items_part =~ s{\0$}{} unless $commit_part;
+
+            my ( $items_info, $items_stat ) = $self->parse_items_log_line( $items_part );
+            if ( ref $items_info ne 'ARRAY' ) {
+                $err_msg = "Item line error. $items_info.";
                 last PARSE_LOG;
             }
-            push @{$commit->{items}}, $item_info;
-            $ac_state = 'item';
-            next;
+            $commit->{items} = $items_info;
+            $commit->{stat} = $items_stat;
+
+            # end of log
+            last PARSE_LOG unless defined $commit_part;
+
+            $ac_state = 'commit_after_items';
+            $line = $commit_part;
+            goto PARSE_COMMIT_LINE;
         }
 
         # empty_af 1x - code is above
@@ -274,12 +334,12 @@ sub get_log {
         $err_msg = "Can't determine line type";
         last PARSE_LOG;
     }
-    
+
     if ( $err_msg ) {
         print "Parsing error on line $line_num: $err_msg\n";
     }
 
-    my $err = $cmd->stderr(); 
+    my $err = $cmd->stderr();
     my $err_out = do { local $/; <$err> };
     if ( $err_out ) {
         $self->{err_msg} = "Error:\n  $err_out\n" . $err_msg;
@@ -311,10 +371,10 @@ sub get_err_msg {
 
 sub get_refs {
     my ( $self, $filter_type ) = @_;
-    
+
     my $cmd = $self->{repo}->command( 'for-each-ref' );
     print "LogRaw cmdline: '" . join(' ', $cmd->cmdline() ) . "'\n" if $self->{ver} >= 4;
-    
+
     my $line_num = 0;
     my $refs = {};
     my $err_msg = undef;
@@ -345,10 +405,10 @@ sub get_refs {
                     $ref_info->{type} = 'unknown';
                     $ref_info->{name_base} = $name_base;
                 }
-                
+
                 next if defined $filter_type && $filter_type ne $ref_info->{type};
                 $refs->{ $tag_name } = $ref_info;
-            
+
             } else {
                 $err_msg = "Can't split ref name '$tag_name' to parts";
                 last PARSE_REF;
@@ -360,12 +420,12 @@ sub get_refs {
         $err_msg = "Can't parse line";
         last PARSE_REF;
     }
-    
+
     if ( $err_msg ) {
         print "Parsing error on line $line_num: $err_msg\n";
     }
 
-    my $err = $cmd->stderr(); 
+    my $err = $cmd->stderr();
     my $err_out = do { local $/; <$err> };
     if ( $err_out ) {
         $self->{err_msg} = "Error:\n  $err_out\n" . $err_msg;
